7f8f9e53bafd89543fd9f19f81d7ab24f5ba8345
Bogdan Stolojan <stolojan.bogdan@gmail.com>
2019-08-29 16:42:52 +0100

    Basic implementation of the Client
    A basic implementation of the client which can create requests and also
    Do them

--- client.go
+++ client.go
@@ -1,1 +1,62 @@
 package bogthesrc
+
+import (
+	"net/http"
+	"net/url"
+)
+
+// define Client struct
+type Client struct {
+	BaseUrl   *url.URL
+	UserAgent string
+
+	httpClient *http.Client
+}
+
+const (
+	version   = "0.0.1"
+	userAgent = "bogthesrc-client" + version
+)
+
+// NewClient
+func NewClient(client *http.Client) *Client {
+	if client == nil {
+		client = http.DefaultClient
+	}
+
+	return &Client{
+		BaseUrl:    &url.URL{Scheme: "http", Host: "bogthesrc.co.uk", Path: "/api"},
+		UserAgent:  userAgent,
+		httpClient: client,
+	}
+}
+
+// client.NewRequest - creates a new request http.NewRequest for method, relativeUrl, jsonBody?
+func (c *Client) NewRequest(method, relativeUrl string) (*http.Request, error) {
+	rel, err := url.Parse(relativeUrl)
+	if err != nil {
+		return nil, err
+	}
+
+	u := c.BaseUrl.ResolveReference(rel)
+
+	req, err := http.NewRequest(method, u.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("User-Agent", c.UserAgent)
+	return req, nil
+}
+
+// client.Do - sends an API request and receives an API response, returns the response or error
+func (c *Client) Do(request *http.Request) (*http.Response, error) {
+	resp, err := c.httpClient.Do(request)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	return resp, nil
+}
--- client_test.go
+++ client_test.go
@@ -1,1 +1,25 @@
 package bogthesrc
+
+import (
+	"testing"
+)
+
+func TestClientBasics(t *testing.T) {
+	t.Run("Can create a new request", func(t *testing.T) {
+		c := NewClient(nil)
+		req, err := c.NewRequest("GET", "posts")
+		ua := req.Header.Get("User-Agent")
+
+		if req.Method != "GET" {
+			t.Errorf("Unexpected method, got: %s", req.Method)
+		}
+
+		if ua != userAgent {
+			t.Errorf("Unexpected UA, got: %s", ua)
+		}
+
+		if err != nil {
+			t.Errorf("Failed with: %s", err)
+		}
+	})
+}
