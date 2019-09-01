eea93f099e5011926fcaedeb2b02737815064ec7
Bogdan Stolojan <stolojan.bogdan@gmail.com>
2019-09-01 19:42:48 +0100

    Form creation for post
    Rejoyce fellow people! We are now civilized, we can now use a form
    `/submit` to submit posts into our system! We have reached a point where
    our system architecture has "saturated", and now we're extending
    previously defined work flows. (I don't know if this makes sense)
    basically, no more new suprises, everything that gets added from now on
    will look very similar to what it's already there.

--- app/post.go
+++ app/post.go
@@ -2,10 +2,14 @@ package app
 
 import (
 	"net/http"
+	"net/url"
 	"strconv"
+	"strings"
 
 	"github.com/bogthe/bogthesrc"
+	"github.com/bogthe/bogthesrc/router"
 	"github.com/gorilla/mux"
+	"github.com/gorilla/schema"
 )
 
 var apiClient = bogthesrc.NewApiClient(nil)
@@ -39,4 +43,53 @@ func servePosts(w http.ResponseWriter, r *http.Request) error {
 	}{
 		posts,
 	})
+}
+
+func servePostCreate(w http.ResponseWriter, r *http.Request) error {
+	if err := r.ParseForm(); err != nil {
+		return err
+	}
+
+	schemaDecoder := schema.NewDecoder()
+
+	var post bogthesrc.Post
+	if err := schemaDecoder.Decode(&post, r.Form); err != nil {
+		return err
+	}
+
+	if err := apiClient.Posts.Create(&post); err != nil {
+		return err
+	}
+
+	redirectUrl := urlTo(router.Post, "ID", strconv.Itoa(post.ID))
+	http.Redirect(w, r, redirectUrl.String(), http.StatusSeeOther)
+
+	return nil
+}
+
+func servePostCreateForm(w http.ResponseWriter, r *http.Request) error {
+	q := r.URL.Query()
+
+	post := &bogthesrc.Post{
+		Title: getValueOrDefault(q, "Title"),
+		Link:  getValueOrDefault(q, "Link"),
+		Body:  getValueOrDefault(q, "Body"),
+	}
+
+	return renderTemplate(w, r, PostCreateTemplate, http.StatusOK, struct {
+		Post         *bogthesrc.Post
+		ActionTarget string
+	}{
+		post,
+		router.PostCreate,
+	})
+}
+
+func getValueOrDefault(q url.Values, name string) string {
+	if val, success := q[name]; success {
+		return val[0]
+	}
+
+	// url.Values is case-sensitive
+	return q.Get(strings.ToLower(name))
 }
--- router/app.go
+++ router/app.go
@@ -2,10 +2,16 @@ package router
 
 import "github.com/gorilla/mux"
 
+const (
+	PostCreateForm = "post:create-form"
+)
+
 func App() *mux.Router {
 	r := mux.NewRouter()
 	r.Path("/").Methods("GET").Name(Home)
 	r.Path("/posts").Methods("GET").Name(Posts)
+	r.Path("/submit").Methods("GET").Name(PostCreateForm)
+	r.Path("/posts").Methods("POST").Name(PostCreate)
 	r.Path("/posts/{ID:.+}").Methods("GET").Name(Post)
 
 	return r
